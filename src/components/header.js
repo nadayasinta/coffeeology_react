cf09890bd5507ff9c622dabb0cd01f5c072e6157
farizap <fapriyanto@alterra.id>
2019-09-11 00:15:29 +0700

    add header component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(props) {
+  return;
+  <div className="container-fluid header">
+    <div className="row h-100">
+      <div className="col-12 text-center">
+        <h2>Coffeology</h2>
+      </div>
+    </div>
+  </div>;
+}
+
+export default Header;
