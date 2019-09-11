dd2bfc9c4a74c9cf6156caae94639e5faf79219b
farizap <fapriyanto@alterra.id>
2019-09-11 15:04:07 +0700

    add methodCard component

---
+++ src/components/methodCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function methodCard(props) {
+  return;
+  <div className="container methodCard">
+    <div className="row h-100">
+      <div className="col-12 text-center">
+        <img src={props.icon} />
+        <h6>{props.name}</h6>
+      </div>
+    </div>
+  </div>;
+}
+
+export default methodCard;
