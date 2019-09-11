2f24cb699f12aa40b5a7bdfdc2a5ce82000c837e
nadayasinta <nada@alterra.id>
2019-09-11 14:59:57 +0700

    nada-navbar update image

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,9 @@
 import React from "react";
-import me from "../assets/images/navbarMe.png";
+import navbarProfile from "../assets/images/navbarProfile.png";
+import navbarSearch from "../assets/images/navbarSearch.png";
+import navbarBeans from "../assets/images/navbarBeans.png";
+import navbarBrew from "../assets/images/navbarBrew.png";
+import navbarActivity from "../assets/images/navbarActivity.png";
 import { Link } from "react-router-dom";
 
 function Navbar(props) {
@@ -9,28 +13,28 @@ function Navbar(props) {
                 <div className="row justify-content-center">
                     <div className="col-2">
                         <Link to={"/"}>
-                            <img src={me} className="navbarLogo" />
+                            <img src={navbarBrew} className="navbarLogo" />
                             <br />
                             Seduh
                         </Link>
                     </div>
                     <div className="col-2">
-                        <img src={me} className="navbarLogo" />
+                        <img src={navbarBeans} className="navbarLogo" />
                         <br />
                         Biji
                     </div>
                     <div className="col-2">
-                        <img src={me} className="navbarLogo" />
+                        <img src={navbarSearch} className="navbarLogo" />
                         <br />
                         Cari
                     </div>
                     <div className="col-2">
-                        <img src={me} className="navbarLogo" />
+                        <img src={navbarActivity} className="navbarLogo" />
                         <br />
                         Aktivitas
                     </div>
                     <div className="col-2">
-                        <img src={me} className="navbarLogo" />
+                        <img src={navbarProfile} className="navbarLogo" />
                         <br />
                         Profile
                     </div>
