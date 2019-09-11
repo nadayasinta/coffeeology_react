4acc82cd0ad86e2833313d955e7ca9677cf125c2
nadayasinta <nada@alterra.id>
2019-09-11 16:39:58 +0700

    add pahe brewing

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 function Header(props) {
-  return;
-  <div className="container-fluid header">
-    <div className="row h-100">
-      <div className="col-12 text-center">
-        <h2>Coffeology</h2>
-      </div>
-    </div>
-  </div>;
+  return (
+      <div className="container-fluid header px-0">
+          <div className="row h-100">
+              <div className="col-12 text-center">
+                  <h2>Coffeology</h2>
+                </div>
+            </div>
+        </div>
+  );
 }
 
 export default Header;
