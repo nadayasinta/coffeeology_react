99033351603a7a0e9ad847af3cc223f71bbb887a
nadayasinta <nada@alterra.id>
2019-09-11 17:00:14 +0700

    brewing page finish

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,40 +9,40 @@ import navbarActivity from '../assets/images/navbarActivity.png';
 
 function Navbar(props) {
   return (
-    <div className="container-fluid navbar shadow px-0 py-3">
-      <div className="row justify-content-center">
-        <div className="col-2 text-center px-0">
-          <Link to="/">
-            <img src={navbarBrew} className="navbarIcon w-50" />
-            <h6>Seduh</h6>
-          </Link>
+      <div className="container-fluid navbar shadow px-0 py-3">
+          <div className="row justify-content-center">
+              <div className="col-2 text-center px-0">
+                  <Link to="/">
+                      <img src={navbarBrew} className="navbarIcon w-50" />
+                      <h6>Seduh</h6>
+                    </Link>
+                </div>
+              <div className="col-2 text-center px-0">
+                  <Link to="/">
+                      <img src={navbarBeans} className="navbarIcon w-50" />
+                      <h6>Biji</h6>
+                    </Link>
+                </div>
+              <div className="col-2 text-center px-0">
+                  <Link to="/">
+                      <img src={navbarSearch} className="navbarIcon w-50" />
+                      <h6>Cari</h6>
+                    </Link>
+                </div>
+              <div className="col-2 text-center px-0">
+                  <Link to="/">
+                      <img src={navbarActivity} className="navbarIcon w-50" />
+                      <h6>Aktivitas</h6>
+                    </Link>
+                </div>
+              <div className="col-2 text-center px-0">
+                  <Link to="/">
+                      <img src={navbarProfile} className="navbarIcon  w-50" />
+                      <h6>Profile</h6>
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className="col-2 text-center px-0">
-          <Link to="/">
-            <img src={navbarBeans} className="navbarIcon w-50" />
-            <h6>Biji</h6>
-          </Link>
-        </div>
-        <div className="col-2 text-center px-0">
-          <Link to="/">
-            <img src={navbarSearch} className="navbarIcon w-50" />
-            <h6>Cari</h6>
-          </Link>
-        </div>
-        <div className="col-2 text-center px-0">
-          <Link to="/">
-            <img src={navbarActivity} className="navbarIcon w-50" />
-            <h6>Aktivitas</h6>
-          </Link>
-        </div>
-        <div className="col-2 text-center px-0">
-          <Link to="/">
-            <img src={navbarProfile} className="navbarIcon  w-50" />
-            <h6>Profile</h6>
-          </Link>
-        </div>
-      </div>
-    </div>
   );
 }
 
--- src/pages/brewing.js
+++ src/pages/brewing.js
@@ -7,22 +7,22 @@ import Navbar from '../components/navbar';
 class Brewing extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-        <div className="container">
-          <div className="row">
-            {this.props.methods.map((method, index) => (
-              <div className="col-4 py-3">
-                <MethodCard
-                  name={method.name}
-                  icon={method.icon}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        <Navbar />
-      </div>
+          <div>
+              <Header />
+              <div className="container">
+                  <div className="row">
+                      {this.props.methods.map((method, index) => (
+                          <div className="col-4 py-3">
+                              <MethodCard
+                                  name={method.name}
+                                  icon={method.icon}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+              <Navbar />
+            </div>
     );
   }
 }
