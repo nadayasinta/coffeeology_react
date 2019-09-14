90bfd517b35be303269a5fb34c419567b96803f0
adesupraptolaia <ades@alterra.id>
2019-09-14 22:16:13 +0700

    tes

--- src/components/history.js
+++ src/components/history.js
@@ -1,7 +1,21 @@
 import React from "react";
+import RecipeCard from "./recipeCard";
+import { connect } from "unistore/react";
+import actionsRecipes from "../store/actionsRecipes";
 
 function history(props) {
-  return <div className="container-fluid history border">History</div>;
+  return (
+    <div>
+      {props.recipes.map(value => (
+        <div className="col-12">
+          <RecipeCard data={value} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default history;
+export default connect(
+  "recipes, stepTypes, recipeDetails",
+  actionsRecipes
+)(history);
--- src/store/store.js
+++ src/store/store.js
@@ -21,32 +21,32 @@ const Toast = Swal.mixin({
 const initialState = {
   // data recipe
   recipes: [
-    // {
-    //   name: "Ultimate v60",
-    //   methodID: 1,
-    //   beanName: "Beans Gayoo Bourbon",
-    //   beanProcess: "Full Wash",
-    //   beanRoasting: "Medium",
-    //   rating: 45,
-    //   favoriteCount: 20,
-    //   time: 100,
-    //   coffeeWeight: 17,
-    //   water: 200,
-    //   icon: require("../assets/images/StepIcon/stir.png")
-    // },
-    // {
-    //   name: "Ultimate v60 2",
-    //   methodID: 1,
-    //   beanName: "Beans Gayoo Bourbon",
-    //   beanProcess: "Full Wash",
-    //   beanRoasting: "Medium",
-    //   rating: 4.5,
-    //   favoriteCount: 20,
-    //   time: 100,
-    //   coffeeWeight: 17,
-    //   water: 200,
-    //   icon: require("../assets/images/StepIcon/stir.png")
-    // }
+    {
+      name: "Ultimate v60",
+      methodID: 1,
+      beanName: "Beans Gayoo Bourbon",
+      beanProcess: "Full Wash",
+      beanRoasting: "Medium",
+      rating: 45,
+      favoriteCount: 20,
+      time: 100,
+      coffeeWeight: 17,
+      water: 200,
+      icon: require("../assets/images/StepIcon/stir.png")
+    },
+    {
+      name: "Ultimate v60 2",
+      methodID: 1,
+      beanName: "Beans Gayoo Bourbon",
+      beanProcess: "Full Wash",
+      beanRoasting: "Medium",
+      rating: 4.5,
+      favoriteCount: 20,
+      time: 100,
+      coffeeWeight: 17,
+      water: 200,
+      icon: require("../assets/images/StepIcon/stir.png")
+    }
   ],
   recipe: null,
   // name: "Ultimate v60",
