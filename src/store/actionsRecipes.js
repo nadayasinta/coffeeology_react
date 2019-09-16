b576b8109f35c5e546c64be8a5c47725a0fdd931
farizap <fapriyanto@alterra.id>
2019-09-17 00:23:29 +0700

    add method to request search recipe

--- src/store/actionsRecipes.js
+++ src/store/actionsRecipes.js
@@ -34,6 +34,9 @@ const actionsRecipes = store => ({
   setSearchParams(state, value) {
     return { searchParams: value };
   },
+  setSearchKeyword(state, value) {
+    return { searchKeyword: value };
+  },
 
   // axios
 
@@ -76,6 +79,23 @@ const actionsRecipes = store => ({
       store.setState({ recipes: response.data.data });
     });
   },
+  async searchRecipes(state, filterParams, searchParams) {
+    console.log(filterParams);
+    console.log(searchParams);
+    const paramsInput = filterParams;
+    paramsInput["search"] = searchParams;
+    let config = {
+      method: "get",
+      url: store.getState().baseURL + "/recipes",
+      params: paramsInput
+    };
+
+    await axios(config).then(response => {
+      console.log(response);
+      store.setState({ recipesSearch: response.data.data });
+    });
+  },
+
   // async getRecipes(state, paramsInput = null) {
   //   let config = {
   //     method: "get",
