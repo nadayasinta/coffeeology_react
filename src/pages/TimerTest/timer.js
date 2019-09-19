fd52e9c957dca0b1614a4733a2b9f766e0656c74
nadayasinta <nada@alterra.id>
2019-09-20 02:29:48 +0700

    styling

--- src/pages/TimerTest/timer.js
+++ src/pages/TimerTest/timer.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Timer(props) {
-  return (
-    <h1>
-      Time : {Math.floor(Math.floor(props.timerNow / 10) / 60)}.{" "}
-      {Math.floor(props.timerNow / 10) % 60}. {props.timerNow % 10}{" "}
-    </h1>
-  );
+    return (
+        <h1>
+            Time : {Math.floor(Math.floor(props.timerNow / 10) / 60)}.{" "}
+            {Math.floor(props.timerNow / 10) % 60}. {props.timerNow % 10}{" "}
+        </h1>
+    );
 }
 
 export default Timer;
