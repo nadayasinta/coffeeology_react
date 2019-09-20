528d1c7d19e3fd8c4ec82dce4085026b72ae01bb
nadayasinta <nada@alterra.id>
2019-09-20 14:55:32 +0700

    fix

--- src/components/myBrew.js
+++ src/components/myBrew.js
@@ -13,69 +13,78 @@ import RecipeCard from "./recipeCard";
 import loading from "../assets/images/loading.gif";
 
 class MyBrew extends React.Component {
-  convertSeconds(secondsInput) {
-    let minutes = Math.floor(parseInt(secondsInput) / 60);
-    let seconds = parseInt(secondsInput) - minutes * 60;
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
+    convertSeconds(secondsInput) {
+        let minutes = Math.floor(parseInt(secondsInput) / 60);
+        let seconds = parseInt(secondsInput) - minutes * 60;
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+        return `${minutes}:${seconds}`;
     }
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
+
+    componentDidMount = async () => {
+        this.props.getMyBrew();
+    };
+
+    componentWillUnmount = () => {
+        this.props.setMyBrew(null);
+    };
+
+    render() {
+        if (this.props.myBrew === null) {
+            return <img src={loading} alt="loading..." />;
+        } else if (this.props.myBrew.data.length === 0) {
+            return <h4>Anda belum membuat Resep</h4>;
+        } else {
+            return (
+                <div>
+                    <Link to="/recipes/create">
+                        <button className="btn btn-primary">
+                            Tambah Guide
+                        </button>
+                    </Link>
+                    {this.props.myBrew.data.map((value, key) => (
+                        <div className="col-12">
+                            <Link to={`/recipe/${value.id}`}>
+                                <RecipeCard
+                                    className="w-100"
+                                    pageType="pageMyBrew"
+                                    method={
+                                        this.props.methods[value.methodID - 1]
+                                    }
+                                    data={value}
+                                    time={this.convertSeconds(value.time)}
+                                />
+                            </Link>
+                        </div>
+                    ))}
+                    <Pagination size="lg">
+                        {this.props.myBrew.pageNow === 1 ? (
+                            <span></span>
+                        ) : (
+                            <Pagination.First
+                                onClick={this.handlePreviousPageButton}
+                            />
+                        )}
+                        {this.props.myBrew.pageNow ===
+                        this.props.myBrew.pageTotal ? (
+                            <span></span>
+                        ) : (
+                            <Pagination.Last
+                                onClick={this.handleNextPageButton}
+                            />
+                        )}
+                    </Pagination>
+                </div>
+            );
+        }
     }
-    return `${minutes}:${seconds}`;
-  }
-
-  componentDidMount = async () => {
-    this.props.getMyBrew();
-  };
-
-  componentWillUnmount = () => {
-    this.props.setMyBrew(null);
-  };
-
-  render() {
-    if (this.props.myBrew === null) {
-      return <img src={loading} alt="loading..." />;
-    } else if (this.props.myBrew.data.length === 0) {
-      return <h4>Anda belum membuat Resep</h4>;
-    } else {
-      return (
-        <div>
-          <Link to="/recipes/create">
-            <button className="btn btn-primary">Tambah Guide</button>
-          </Link>
-          {this.props.myBrew.data.map((value, key) => (
-            <div className="col-12">
-              <Link to={`/recipe/${value.id}`}>
-                <RecipeCard
-                  className="w-100"
-                  pageType="pageMyBrew"
-                  methodIcon={this.props.methods[value.methodID - 1].icon}
-                  data={value}
-                  time={this.convertSeconds(value.time)}
-                />
-              </Link>
-            </div>
-          ))}
-          <Pagination size="lg">
-            {this.props.myBrew.pageNow === 1 ? (
-              <span></span>
-            ) : (
-              <Pagination.First onClick={this.handlePreviousPageButton} />
-            )}
-            {this.props.myBrew.pageNow === this.props.myBrew.pageTotal ? (
-              <span></span>
-            ) : (
-              <Pagination.Last onClick={this.handleNextPageButton} />
-            )}
-          </Pagination>
-        </div>
-      );
-    }
-  }
 }
 
 export default connect(
-  "myBrew, methods",
-  actionsActivity
+    "myBrew, methods",
+    actionsActivity
 )(MyBrew);
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -24,190 +24,201 @@ import loading from "../assets/images/loading.gif";
 const _ = require("lodash");
 
 const useStyles = makeStyles({
-  list: {
-    width: 250
-  },
-  fullList: {
-    width: "auto",
-    maxWidth: "480px"
-  }
+    list: {
+        width: 250
+    },
+    fullList: {
+        width: "auto",
+        maxWidth: "480px"
+    }
 });
 
 const useStylesSearch = makeStyles(theme => ({
-  root: {
-    padding: "2px 4px",
-    display: "flex",
-    alignItems: "center",
-    width: "100%"
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1
-  },
-  iconButton: {
-    padding: 10
-  },
-  divider: {
-    height: 28,
-    margin: 4
-  }
+    root: {
+        padding: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: "100%"
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1
+    },
+    iconButton: {
+        padding: 10
+    },
+    divider: {
+        height: 28,
+        margin: 4
+    }
 }));
 
 const Search = props => {
-  const classes = useStyles();
-  const classesSearch = useStylesSearch();
+    const classes = useStyles();
+    const classesSearch = useStylesSearch();
 
-  const [state, setState] = React.useState({
-    bottom: false,
-    pagination: 1
-  });
+    const [state, setState] = React.useState({
+        bottom: false,
+        pagination: 1
+    });
 
-  const toggleDrawer = (side, open) => event => {
-    if (
-      event &&
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
+    const toggleDrawer = (side, open) => event => {
+        if (
+            event &&
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
 
-    setState({ ...state, bottom: open });
-  };
-
-  const fullList = side => (
-    <div
-      className={classes.fullList}
-      role="presentation"
-      // onClick={toggleDrawer(side, false)}
-      // onKeyDown={toggleDrawer(side, false)}
-    >
-      <Filter onClick={toggleDrawer("bottom", false)} />
-    </div>
-  );
-
-  const onSubmitSearch = event => {
-    event.preventDefault();
-    props.searchRecipes(
-      props.searchParams,
-      props.searchKeyword,
-      state.pagination
-    );
-  };
-  const onChangeSearch = event => {
-    event.preventDefault();
-    props.setSearchKeyword(event.target.value);
-  };
-
-  React.useEffect(() => {
-    props.searchRecipes(
-      props.searchParams,
-      props.searchKeyword,
-      state.pagination
-    );
-
-    return () => {
-      props.setRecipesSearch(null);
+        setState({ ...state, bottom: open });
     };
-  }, [props.searchParams, state.pagination]);
 
-  const convertSeconds = secondsInput => {
-    let minutes = Math.floor(parseInt(secondsInput) / 60);
-    let seconds = parseInt(secondsInput) - minutes * 60;
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
-    return `${minutes}:${seconds}`;
-  };
+    const fullList = side => (
+        <div
+            className={classes.fullList}
+            role="presentation"
+            // onClick={toggleDrawer(side, false)}
+            // onKeyDown={toggleDrawer(side, false)}
+        >
+            <Filter onClick={toggleDrawer("bottom", false)} />
+        </div>
+    );
 
-  const handlePreviousPageButton = event => {
-    event.preventDefault();
-    setState({ pagination: state.pagination - 1 });
-  };
-  const handleNextPageButton = event => {
-    event.preventDefault();
-    setState({ ...state, pagination: state.pagination + 1 });
-  };
+    const onSubmitSearch = event => {
+        event.preventDefault();
+        props.searchRecipes(
+            props.searchParams,
+            props.searchKeyword,
+            state.pagination
+        );
+    };
+    const onChangeSearch = event => {
+        event.preventDefault();
+        props.setSearchKeyword(event.target.value);
+    };
 
-  return (
-    <div>
-      <form onSubmit={onSubmitSearch}>
-        <Paper className={classesSearch.root}>
-          <IconButton className={classesSearch.iconButton} aria-label="menu">
-            <MenuIcon onClick={toggleDrawer("bottom", true)} />
-          </IconButton>
-          <Divider className={classesSearch.divider} orientation="vertical" />
-          <InputBase
-            className={classesSearch.input}
-            placeholder="Cari Nama Guides atau Nama Biji Kopi"
-            inputProps={{ "aria-label": "search google maps" }}
-            onChange={onChangeSearch}
-          />
-          <IconButton
-            className={classesSearch.iconButton}
-            aria-label="search"
-            type="submit"
-          >
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-      </form>
-      {props.recipesSearch === null ? (
-        <img src={loading} alt="loading" />
-      ) : _.isEmpty(props.recipesSearch.recipes) ? (
-        <h5>Maaf, Resep yang anda cari tidak ada</h5>
-      ) : (
-        props.recipesSearch.recipes.map(value => {
-          return (
-            <div className="col-12">
-              <Link to={"/recipe/" + value.id}>
-                <RecipeCard
-                  data={value}
-                  methodIcon={props.methods[value.methodID - 1].icon}
-                  time={convertSeconds(value.time)}
-                />
-              </Link>
-            </div>
-          );
-        })
-      )}
-      <br />
-      {!_.isEmpty(props.recipesSearch) ? (
-        <Pagination size="lg" className="justify-content-between">
-          {props.recipesSearch.pageNow === 1 ? (
-            <span></span>
-          ) : (
-            <div className="text-center">
-              <Pagination.First onClick={handlePreviousPageButton} />
-            </div>
-          )}
-          {props.recipesSearch.pageNow === props.recipesSearch.pageTotal ? (
-            <span></span>
-          ) : (
-            <div className="text-center">
-              <Pagination.Last onClick={handleNextPageButton} />
-            </div>
-          )}
-        </Pagination>
-      ) : (
-        <div></div>
-      )}
+    React.useEffect(() => {
+        props.searchRecipes(
+            props.searchParams,
+            props.searchKeyword,
+            state.pagination
+        );
 
-      <SwipeableDrawer
-        anchor="bottom"
-        open={state.bottom}
-        onClose={toggleDrawer("bottom", false)}
-        onOpen={toggleDrawer("bottom", true)}
-      >
-        {fullList("bottom")}
-      </SwipeableDrawer>
-    </div>
-  );
+        return () => {
+            props.setRecipesSearch(null);
+        };
+    }, [props.searchParams, state.pagination]);
+
+    const convertSeconds = secondsInput => {
+        let minutes = Math.floor(parseInt(secondsInput) / 60);
+        let seconds = parseInt(secondsInput) - minutes * 60;
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+        return `${minutes}:${seconds}`;
+    };
+
+    const handlePreviousPageButton = event => {
+        event.preventDefault();
+        setState({ pagination: state.pagination - 1 });
+    };
+    const handleNextPageButton = event => {
+        event.preventDefault();
+        setState({ ...state, pagination: state.pagination + 1 });
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmitSearch}>
+                <Paper className={classesSearch.root}>
+                    <IconButton
+                        className={classesSearch.iconButton}
+                        aria-label="menu"
+                    >
+                        <MenuIcon onClick={toggleDrawer("bottom", true)} />
+                    </IconButton>
+                    <Divider
+                        className={classesSearch.divider}
+                        orientation="vertical"
+                    />
+                    <InputBase
+                        className={classesSearch.input}
+                        placeholder="Cari Nama Guides atau Nama Biji Kopi"
+                        inputProps={{ "aria-label": "search google maps" }}
+                        onChange={onChangeSearch}
+                    />
+                    <IconButton
+                        className={classesSearch.iconButton}
+                        aria-label="search"
+                        type="submit"
+                    >
+                        <SearchIcon />
+                    </IconButton>
+                </Paper>
+            </form>
+            {props.recipesSearch === null ? (
+                <img src={loading} alt="loading" />
+            ) : _.isEmpty(props.recipesSearch.recipes) ? (
+                <h5>Maaf, Resep yang anda cari tidak ada</h5>
+            ) : (
+                props.recipesSearch.recipes.map(value => {
+                    return (
+                        <div className="col-12">
+                            <Link to={"/recipe/" + value.id}>
+                                <RecipeCard
+                                    data={value}
+                                    method={
+                                        props.methods[value.methodID - 1].icon
+                                    }
+                                    time={convertSeconds(value.time)}
+                                />
+                            </Link>
+                        </div>
+                    );
+                })
+            )}
+            <br />
+            {!_.isEmpty(props.recipesSearch) ? (
+                <Pagination size="lg" className="justify-content-between">
+                    {props.recipesSearch.pageNow === 1 ? (
+                        <span></span>
+                    ) : (
+                        <div className="text-center">
+                            <Pagination.First
+                                onClick={handlePreviousPageButton}
+                            />
+                        </div>
+                    )}
+                    {props.recipesSearch.pageNow ===
+                    props.recipesSearch.pageTotal ? (
+                        <span></span>
+                    ) : (
+                        <div className="text-center">
+                            <Pagination.Last onClick={handleNextPageButton} />
+                        </div>
+                    )}
+                </Pagination>
+            ) : (
+                <div></div>
+            )}
+
+            <SwipeableDrawer
+                anchor="bottom"
+                open={state.bottom}
+                onClose={toggleDrawer("bottom", false)}
+                onOpen={toggleDrawer("bottom", true)}
+            >
+                {fullList("bottom")}
+            </SwipeableDrawer>
+        </div>
+    );
 };
 
 export default connect(
-  "searchParams,searchKeyword,recipesSearch,methods",
-  actionsRecipes
+    "searchParams,searchKeyword,recipesSearch,methods",
+    actionsRecipes
 )(Search);
