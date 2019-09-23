1de2751db8da95569cce550547762d1d6cd51d89
nadayasinta <nada@alterra.id>
2019-09-23 10:19:13 +0700

    styling lagi

--- src/components/MethodCard.js
+++ src/components/MethodCard.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 function methodCard(props) {
   return (
-    <div className="container-fluid methodcard border rounded-lg">
-      <div className="row">
-        <div className="col-12   text-center px-2 py-1">
-          <img src={props.icon} className="w-100 mb-2" alt="altTag" />
-          <div className=" methodname align-content-center m-auto text-center">
-            <h6 className="m-0">{props.name}</h6>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div className="container-fluid methodcard">
+  <div className="row">
+  <div className="col-12  border rounded text-center px-2 py-1 mb-1">
+  <img src={props.icon} className="w-100 mb-2 pt-2" alt="altTag" />
+				</div>
+  <div className="col-12 p-0 methodname align-content-center text-center">
+  <h6 className="m-0 py-2 border-top border-bottom">{props.name}</h6>
+				</div>
+			</div>
+		</div>
   );
 }
 
